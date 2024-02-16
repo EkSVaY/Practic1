ae9139070c2f52af6eeeeb548fe5edba409356d6
EkSVaY <v.ekin@g.nsu.ru>
2024-02-16 13:31:23 +0700

    3 - 9

---
+++ 9.py
@@ -0,0 +1,7 @@
+print("Введите плей-лист папы:")
+song_1 = input()
+song_2 = input()
+song_3 = input()
+song_4 = input()
+song_5 = input()
+print(f"Плей-лист мамы: \n{song_5} \n{song_4} \n{song_3} \n{song_2} \n{song_1} ")
