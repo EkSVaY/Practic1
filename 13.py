9c82f17da337824095941045456f92a4f1216976
EkSVaY <159280377+EkSVaY@users.noreply.github.com>
2024-02-17 08:27:59 +0700

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,4 +1,4 @@
 import math
 r_blind = float(input())
 signal_r = float(input())
-print(abs((math.pi * 2 * signal_r ** 2)/2 - (math.pi * 2 * r_blind ** 2)/2))
+print(abs((math.pi * signal_r ** 2) - (math.pi * r_blind ** 2)))
